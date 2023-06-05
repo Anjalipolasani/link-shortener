85c86f430a4914ae2bea027789c7f7194e9edd6a
Anjalipolasani <102357610+Anjalipolasani@users.noreply.github.com>
2023-06-05 16:35:52 +0530

    Add files via upload

---
+++ copy.js
@@ -0,0 +1,6 @@
+let newURL = document.getElementById("shorturl");
+let coptButton = document.getElementById("copy");
+coptButton.onclick = ()=>{
+    newURL.select();
+    window.navigator.clipboard.writeText(newURL.value);
+}
